dcb42803cc3a379a620ae75b6a8e9267b063d483
Hardwaregore <hardwaregore101@gmail.com>
2022-05-05 12:19:20 -0500

    a

--- init.sql
+++ init.sql
@@ -3,4 +3,9 @@ CREATE DATABASE uwf;
 USE uwf;
 
 CREATE TABLE gild (
+    id int AUTO_INCREMENT NOT NULL,
+    `owner` varcher(255) NOT NULL,
+    amount int NOT NULL,
+    purchased_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
     
